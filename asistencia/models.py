51b22a0347da4b0eb4d527bfbc6b0a83de486b3a
PatrickOvi <dinokillersyt@gmail.com>
2024-11-19 21:18:16 -0300

    creacion de modelos y def de registrar

--- asistencia/models.py
+++ asistencia/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Ingles(models.MODEL):
+    usuario = models.CharField(max_length=20)
+    horas = models.IntegerField()
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Lenguaje(models.MODEL):
+    usuario = models.CharField(max_length=20)
+    horas = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- asistencia/views.py
+++ asistencia/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Ingles, Lenguaje
 
 # Create your views here.
 def index (request):
     return render(request, 'index.html')
+
+def registrar_ingles (request):
+    if(request.method=='POST'):
+        try:
+            usuario =request.POST.get('usuario')
+            horas =request.POST.get('horas')
+
+            ingles = Ingles(
+                usuario = usuario,
+                horas = horas
+            )
+            ingles.save()
+        except Exception as ex:
+            print(f"error: {ex}")
+
+    return redirect('index') 
+
+def registrar_lenguaje (request):
+    if(request.method=='POST'):
+        try:
+            usuario =request.POST.get('usuario')
+            horas =request.POST.get('horas')
+
+            ingles = Lenguaje(
+                usuario = usuario,
+                horas = horas
+            )
+            ingles.save()
+        except Exception as ex:
+            print(f"error: {ex}")
+            
+    return redirect('index') 
